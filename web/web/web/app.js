32907fa89015079b51330e772f4d866bc066fa11
Hafiz Wibisono <hafiizwibisono@gmail.com>
2026-02-02 10:39:34 +0700

    Delete web directory

--- web/web/web/app.js
+++
@@ -1,9 +0,0 @@
-function addProject() {
-    const projectList = document.getElementById("project-list");
-    const newProject = prompt("Masukkan nama project baru:");
-    if (newProject) {
-        const li = document.createElement("li");
-        li.innerText = newProject;
-        projectList.appendChild(li);
-    }
-}
